cbe5a096abc6811f7cf30ff146678009b5d876c3
technicalatg <119067757+technicalatg@users.noreply.github.com>
2024-04-23 23:14:25 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $SOURCE_CODE ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/technicalatg/atgff2.0.git /atgff2.0
+else
+  echo "Cloning Custom Repo from $SOURCE_CODE "
+  git clone $SOURCE_CODE /atgff2.0
+fi
+cd /atgff2.0
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 -m main
